86cb5902d4a9fb9dea0e2e8c0eb648a69820989b
SAHILDWIVEDI1232 <168732444+SAHILDWIVEDI1232@users.noreply.github.com>
2025-08-31 17:09:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+document.addEventListener("DOMLoadContent",function(){;
+const line=document.getElementById('line');
+let angle=0;
+					set Interval(()=>{
+														  angle=+2;
+										let.style.transform=`rotate(${angle}deg)`;},20);
+});
+																									  
+										
+													  
